d75a96ded04db86531560fde744f808d6ca96aa7
Admin <patrickemaher@yahoo.com>
2018-02-17 23:20:40 -0800

    Adding comments to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,17 @@ module.exports = {
     cache: true,
     debug: true,
     output: {
+    		//in node.js __dirname is the directory in which the currently executing script resides
         path: __dirname,
         filename: './src/main/resources/static/built/bundle.js'
     },
     module: {
         loaders: [
             {
+            		//identifies which file(s) should be transformed
                 test: path.join(__dirname, '.'),
                 exclude: /(node_modules)/,
+                //trasform it using babel before adding this file to the bundle
                 loader: 'babel',
                 query: {
                     cacheDirectory: true,
